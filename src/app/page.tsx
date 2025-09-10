3cf4a9802da9a62dbe2444c2fa31982277de2447
Denis <Nigazzz2000@gmail.com>
2025-09-10 17:13:07 +0700

    feat: Поменял цвет заднего фона сайта

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold text-center my-[20%]">Maxter Website</h1>
+    <h1>Hello World!</h1>
   );
 }
