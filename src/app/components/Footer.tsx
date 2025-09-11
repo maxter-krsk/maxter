efb48fb267521e5fd560efbc2a50d2f2c247011a
Kirill Khaylenko <alexfrol219@gmail.com>
2025-09-11 14:37:55 +0700

    feat: Сделал скелет футера | Вынес навигационное меню в компонент

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import NavMenu from "./NavMenu";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <ul>
+          <li>
+            <span>digital</span>
+            <span>upgrade</span>
+          </li>
+          <li>
+            <NavMenu />
+            <Link href="mailto:maxter24@yandex.ru">maxter24@yandex.ru</Link>
+            <Link href="tel:+79230198369">+7 (923) 019-83-69</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <p>
+              Агенство коммуникаций Maxter &copy; {new Date().getFullYear()}
+            </p>
+          </li>
+          <li>
+            <Link href="/privacy-policy">Политика конфиденциальности</Link>
+          </li>
+        </ul>
+        <h1>maxter</h1>
+      </div>
+    </footer>
+  );
+}
---
+++ src/app/components/NavMenu.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+export default function NavMenu() {
+  return (
+    <ul className="flex gap-x-4">
+      <li>
+        <Link href="/about" className="text-blue-500 underline">
+          О нас
+        </Link>
+      </li>
+      <li>
+        <Link href="/services" className="text-blue-500 underline">
+          Услуги
+        </Link>
+      </li>
+      <li>
+        <Link href="/contacts" className="text-blue-500 underline">
+          Наши контакты
+        </Link>
+      </li>
+    </ul>
+  );
+}
