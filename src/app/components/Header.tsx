4c9d9ba4416a3f5ff444857b13338cab947e9c50
Kirill Khaylenko <alexfrol219@gmail.com>
2025-09-18 16:38:00 +0700

    feat: Изменил шапку сайта

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,59 +3,42 @@ import Link from "next/link";
 import NavMenuHeader from "./NavMenuHeader";
 import { unbounded } from "@/app/ui/fonts";
 import { Grid } from "@mui/material";
+import line from "@/app/styles/modules/link-underline.module.css";
 export default function Header() {
   return (
-    <header className={`${unbounded.className}`}>
+    <header className="py-3">
       <div className="container">
-        <Grid
-          container
-          spacing="grow"
-          className="min-h-[3rem] border-b-1 border-l-1 border-r-1 border-black"
-        >
-          <Grid className="flex justify-center p-[0.625rem] items-center border-r-1 border-black">
+        <ul className="flex justify-between items-center">
+          <li className="w-[30rem]">
             <Logo />
-          </Grid>
-          <Grid
-            className="border-r-1 border-black flex items-center justify-start pl-3"
-            size="grow"
-          >
-            <span className="uppercase font-bold text-2xl text-[#423060]">
-              maxter
-            </span>
-          </Grid>
-          <Grid
-            className="border-r-1 border-black flex items-center justify-center"
-            size="grow"
-          >
+          </li>
+          <li>
             <nav>
               <NavMenuHeader />
             </nav>
-          </Grid>
-          <Grid
-            className="flex flex-col items-center justify-evenly"
-            // size="grow"
-          >
-            <Link
-              className="uppercase border-b-1 border-black w-full text-center pb-[0.375rem]"
-              href="mailto:maxter24@yandex.ru"
-            >
-              <img className="p-[0.62rem]" src="/icons/ui/email-icon.svg" alt="email иконка" />
+          </li>
+          <li className="flex gap-5 items-center w-[30rem] justify-end">
+            <Link href="mailto:maxter24@yandex.ru">
+              <img
+                className=""
+                src="/icons/ui/email-icon.svg"
+                alt="email иконка"
+              />
             </Link>
             <Link href="tel:+79230198369">
-              <img className="p-[0.62rem]" src="/icons/ui/call-icon.svg" alt="Трубка телефона" />
+              <img
+                className="border-r-2 pr-5"
+                src="/icons/ui/call-icon.svg"
+                alt="Трубка телефона"
+              />
             </Link>
-          </Grid>
-          <Grid
-            size="grow"
-            className="flex items-center justify-center text-center border-l-1 border-black"
-          >
-            <Link href="/" className="uppercase w-full h-full flex items-center justify-center">
-              Оставить
-              <br />
-              заявку
+            <Link href="/" className={`${unbounded.className} ${line["line-link"]}`}
+            data-replace="Оставить заявку"
+            >
+              <span>Оставить заявку</span>
             </Link>
-          </Grid>
-        </Grid>
+          </li>
+        </ul>
       </div>
     </header>
   );
--- src/app/components/NavMenuHeader.tsx
+++ src/app/components/NavMenuHeader.tsx
@@ -2,19 +2,19 @@ import Link from "next/link";
 import { unbounded } from "@/app/ui/fonts";
 export default function NavMenuHeader() {
   return (
-    <ul className="flex gap-x-4">
+    <ul className="flex gap-x-6">
       <li>
-        <Link href="/about" className="uppercase">
+        <Link href="/about" className={`${unbounded.className} font-light`}>
           О нас
         </Link>
       </li>
       <li>
-        <Link href="/services" className="uppercase">
+        <Link href="/services" className={`${unbounded.className} font-light`}>
           Услуги
         </Link>
       </li>
       <li>
-        <Link href="/contacts" className="uppercase">
+        <Link href="/contacts" className={`${unbounded.className} font-light`}>
           Наши контакты
         </Link>
       </li>
