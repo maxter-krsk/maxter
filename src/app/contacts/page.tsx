487e145044e10f1f9390ae655549888bff923ad8
exrsxe <nigazzz2000@gmail.com>
2026-01-30 16:24:00 +0700

    feat: Подключил заглушку ко всем страницам из навигации

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,9 +1,13 @@
+import Plug from "@/app/components/ui/Plug";
+
 export default function ContactsPage() {
   return (
-    <section>
-      <div className="container">
-        <h1>Страница Контакты</h1>
-      </div>
-    </section>
+    <Plug>
+      <section>
+        <div className="container">
+          <h1>Страница Контакты</h1>
+        </div>
+      </section>
+    </Plug>
   );
 }
