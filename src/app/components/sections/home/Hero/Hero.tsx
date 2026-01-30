0ed8c0ef6d81199c2bf0d134885973c36985eab7
exrsxe <nigazzz2000@gmail.com>
2026-01-30 15:34:56 +0700

    feat: Отступ сверху блока

--- src/app/components/sections/home/Hero/Hero.tsx
+++ src/app/components/sections/home/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import { Clock } from "./Clock";
 
 export function Hero() {
   return (
-    <section className="min-h-screen flex items-center border-x border-b border-t lg:border-t-0 border-carbon dark:border-paper mb-100">
+    <section className="min-h-screen flex items-center border-x border-b border-t lg:border-t-0 mt-20 lg:mt-0 border-carbon dark:border-paper mb-100">
       <div className="container">
         <div className="flex flex-col items-center justify-center">
           <h1 className="dark:text-paper text-carbon uppercase text-20 md:text-40 font-medium font-unbounded text-center mb-20 md:mb-50 lg:mb-140">
